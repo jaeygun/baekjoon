e9a8664f530e4e5f380a0584eb4df38b34b322bf
jaeygun <jaeygun@gmail.com>
2024-03-13 16:21:45 +0900

    24511 문제풀이

--- src/DataStructures/_24511/Main.java
+++ src/DataStructures/_24511/Main.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.Stack;
 
 public class Main {
 
@@ -13,58 +12,40 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int qsLength = Integer.parseInt(br.readLine());
 
-        Object[] qsArr = new Object[qsLength];
         String[] qsFlagArr = br.readLine().split(" "); // 0 1 1 0
-        String[] qsElementArr = br.readLine().split(" "); // 1 2 3 4
+        int length = 0;
         for (int i=0; i<qsFlagArr.length; i++) {
-            switch(Integer.parseInt(qsFlagArr[i])) {
-                case 0:
-                    Deque<Integer> queue = new ArrayDeque<Integer>();
-                    queue.add(Integer.parseInt(qsElementArr[i]));
-                    qsArr[i] = queue;
-                    break;
-                case 1:
-                    Stack<Integer> stack = new Stack<Integer>();
-                    stack.add(Integer.parseInt(qsElementArr[i]));
-                    qsArr[i] = stack;
-                    break;
-                }
+            if (Integer.parseInt(qsFlagArr[i]) == 1) {
+                continue;
+            }
+            length++;
+        }
+        
+        String[] qsElementArr = br.readLine().split(" "); // 1 2 3 4
+        Deque<Integer> queue = new ArrayDeque<>();
+        int idx = qsElementArr.length;
+        while (idx > 0) {
+            if (Integer.parseInt(qsFlagArr[idx-1]) == 0) {
+                queue.add(Integer.parseInt(qsElementArr[idx-1]));
+            }
+            idx--;
         }
 
         int inputArrLength = Integer.parseInt(br.readLine());
         int[] inputArr = new int[inputArrLength];
         String[] value = br.readLine().split(" ");
-        for (int j=0; j<inputArrLength; j++) {
-            inputArr[j] = Integer.parseInt(value[j]); // 2 4 7
+        for (int i=0; i<inputArrLength; i++) {
+            inputArr[i] = Integer.parseInt(value[i]); // 2 4 7
         }
 
         int element = 0;
         StringBuilder sb = new StringBuilder();
-        for (int k=0; k<inputArr.length; k++) {
-            element = inputArr[k]; // 2
-            sb.append(test(qsArr, qsFlagArr, element, 0));
+        for (int i=0; i<inputArr.length; i++) {
+            element = inputArr[i];
+            queue.add(element);
+            sb.append(queue.poll());
+            sb.append(" ");
         }
         System.out.println(sb.toString());
-    }
-
-    private static int test(Object[] qsArr, String[] qsFlagArr, int element, int idx) {
-
-        if (idx == qsArr.length) {
-            return element;
-        }
-
-        if (Integer.parseInt(qsFlagArr[idx]) == 0) {
-            Deque<Integer> queue = (Deque<Integer>) qsArr[idx];
-            queue.add(element);
-            element = queue.poll();
-        } else {
-            Stack<Integer> stack = (Stack<Integer>) qsArr[idx];
-            stack.add(element);
-            element = stack.pop();
-        }
-        idx++;
-        test(qsArr, qsFlagArr, element, idx);
-
-        return element;
     }
 }
